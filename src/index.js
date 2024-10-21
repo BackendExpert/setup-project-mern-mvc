ed52a6b5c95df9fe0bfe3990d67411657c2019f1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-21 21:13:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const client_destination = argv.destination || path.join(process.cwd(), '../');
 
 async function RunSetupProject() {
     try{
-        
+        await fs.copy(client_source, client_destination);
+        console.log("The Project Setup Successfully...")
     }
     catch(err){
         console.log("Error white setup Project ", err)
