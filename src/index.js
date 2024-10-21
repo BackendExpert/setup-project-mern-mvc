d8dd469302ddea02c274d64a9d347fb0328f3b33
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-21 21:34:07 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -4,15 +4,25 @@ const fs = require('fs-extra');
 const path = require('path');
 const argv = require('yargs').argv;
 
-const client_source = argv.source || path.join(__dirname, 'docs/client');
-const client_destination = argv.destination || path.join(process.cwd(), '../');
+const client_source = argv.source || path.join(__dirname, '../docs/client');
+const client_destination = argv.destination || path.join(process.cwd(), './');
+
+const server_source = argv.source || path.join(__dirname, '../docs/server');
+const server_destination = argv.destination || path.join(process.cwd(), '../');
 
 async function RunSetupProject() {
     try{
         await fs.copy(client_source, client_destination);
+        await fs.copy(server_source, server_destination);
         console.log("The Project Setup Successfully...")
     }
     catch(err){
         console.log("Error white setup Project ", err)
     }
 }
+
+async function main() {
+    await RunSetupProject();  
+}
+
+main();
