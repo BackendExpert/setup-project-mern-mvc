fa805e886ed5b9fbd0020125cce9cf070568878e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-21 21:10:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ const fs = require('fs-extra');
 const path = require('path');
 const argv = require('yargs').argv;
 
+const client_source = argv.source || path.join(__dirname, 'docs/client');
+const client_destination = argv.destination || path.join(process.cwd(), '../');
+
+async function RunSetupProject() {
+    try{
+
+    }
+    catch(err){
+        console.log("Error white setup Project ", err)
+    }
+}
